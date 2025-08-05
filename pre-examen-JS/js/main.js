9511c5555a411a0541145c96369e76e5dadbffd6
Thiago Ibañez <thiaguchox@gmail.com>
2025-08-05 15:58:37 -0300

    a
    s

--- pre-examen-JS/js/main.js
+++ pre-examen-JS/js/main.js
@@ -2,3 +2,19 @@ function generarToast(){
 
      M.toast({html: 'I am a toast!'});
 }
+/////
+
+function cambiarColor(){
+     var input = document.querySelector("#colorPicker");
+     var body = document.querySelector("body");
+
+     body.style.backgroundColor = input.value;
+
+}
+
+function escribirEnDiv(){
+     var textarea = document.querySelector("#textarea1");
+     var eldiva = document.querySelector("#eldiv");
+
+     eldiva.innerHTML = "<h3>" + textarea.value + "</h3>";
+}
